b9524367124d3b6c406be318f121656d76ea97d4
JRG 2 <jrg2@ubuntu.(none)>
2014-01-06 19:33:46 -0600

    Test changes

--- block_lib/basic_blocks_func.js
+++ block_lib/basic_blocks_func.js
@@ -1,50 +1,16 @@
 var forward_block = function(params){
-    if (params[1].length == 0){
-        params[1].push(params[0].get_xy()[0]);
-        params[1].push(params[0].get_xy()[1]);
-    }
-    params[0].move(20);
+    params[0].move(60);
     x_pos = params[0].get_xy()[0];
     y_pos = params[0].get_xy()[1];
-    params[1].push(x_pos);
-    params[1].push(y_pos);
-
-    if(params[2] != null){
-        params[2].remove();
-    }
-    params[2] = new Kinetic.Line({
-        points: params[1],
-        stroke: 'red',
-        strokeWidth: 3,
-        lineCap: 'round',
-        lineJoin: 'round'
-    });
-    params[0].layer.add(params[2]);
+    params[1].add_point([x_pos, y_pos]);
     params[0].bring_front();
 }
 
 function backward_block(params){
-    if (params[1].length == 0){
-        params[1].push(params[0].get_xy()[0]);
-        params[1].push(params[0].get_xy()[1]);
-    }
-    params[0].move(-20);
+    params[0].move(-60);
     x_pos = params[0].get_xy()[0];
     y_pos = params[0].get_xy()[1];
-    params[1].push(x_pos);
-    params[1].push(y_pos);
-
-    if(params[2] != null){
-        params[2].remove();
-    }
-    params[2] = new Kinetic.Line({
-        points: params[1],
-        stroke: 'red',
-        strokeWidth: 3,
-        lineCap: 'round',
-        lineJoin: 'round'
-    });
-    params[0].layer.add(params[2]);
+    params[1].add_point([x_pos, y_pos]);
     params[0].bring_front();
 }
 
