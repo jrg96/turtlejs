e6cbe631528abc78e94b39e1d9a44b6ae932ac15
JRG 2 <jrg2@ubuntu.(none)>
2014-01-09 15:36:17 -0600

    Diagonal support

--- ta_lib/turtle.js
+++ ta_lib/turtle.js
@@ -19,6 +19,43 @@ Turtle.prototype = {
             coord[1] = pos;
         }else if (Math.abs(this.rotation) == 270){
             coord[0] = -pos;
+        }else{
+            var h = pos;
+            var o = 0;
+            var a = 0;
+            var ang = 0;
+            var sign = +1;
+
+            if (pos < 0){
+                h = Math.abs(pos);
+                sign = -1;
+            }
+
+            if (this.rotation > 0 && this.rotation < 90){
+                ang = 90 - this.rotation;
+            }else if (this.rotation > 90 && this.rotation < 180){
+                ang = 180 - this.rotation;
+            }else if (this.rotation > 180 && this.rotation < 270){
+                ang = 270 - this.rotation;
+            }else if (this.rotation > 270 && this.rotation < 360){
+                ang = this.rotation - 270;
+            }
+            ang = (ang * Math.PI) / 180;
+            o = Math.round(Math.sin(ang) * h);
+            a = Math.round(Math.cos(ang) * h);
+            if (this.rotation > 0 && this.rotation < 90){
+                coord[0] = a * sign;
+                coord[1] = -o * sign;
+            }else if (this.rotation > 90 && this.rotation < 180){
+                coord[0] = o * sign;
+                coord[1] = a * sign;
+            }else if (this.rotation > 180 && this.rotation < 270){
+                coord[0] = -a * sign;
+                coord[1] = o * sign;
+            }else if (this.rotation > 270 && this.rotation < 360){
+                coord[0] = -a * sign;
+                coord[1] = -o * sign;
+            }
         }
         this.img.move_relative(coord);
         this.bring_front();
