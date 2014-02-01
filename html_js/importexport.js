47f281ca5d768a3b78e6f50ad3cdb96fa41f7ab1
JRG 2 <jrg2@ubuntu.(none)>
2014-02-01 11:58:31 -0600

    Basic file read

--- html_js/importexport.js
+++ html_js/importexport.js
@@ -1,8 +1,15 @@
 function onFileSelect(evt) {
     var file = evt.target.files[0];
+    var reader = new FileReader();
+
+    reader.onload = (function(theFile) {
+        return function(e) {
+            alert(e.target.result);
+        };
+    })(file);
 
     if (file.name.match(".ta$")){
-        alert("es archivo ta");
+        reader.readAsText(file);
     }else {
         alert("no es archivo ta");
     }
