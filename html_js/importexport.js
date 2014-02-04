8f42558a510468add22921567d99416c7d42e361
Jorge Gómez <gomezlopez.jorge96@gmail.com>
2014-02-04 11:18:28 -0600

    Import part 5

--- html_js/importexport.js
+++ html_js/importexport.js
@@ -1,11 +1,11 @@
-function onFileSelect(evt, palette_tracker) {
+function onFileSelect(evt, palette_tracker, block_tracker) {
     var file = evt.target.files[0];
     var reader = new FileReader();
 
     reader.onload = (function(theFile) {
         return function(e) {
             var json_obj = JSON.parse(e.target.result);
-            parseTAFile(json_obj, palette_tracker);
+            parseTAFile(json_obj, palette_tracker, block_tracker);
         };
     })(file);
 
@@ -16,11 +16,14 @@ function onFileSelect(evt, palette_tracker) {
     }
 }
 
-function parseTAFile(json, palette_tracker) {
+function parseTAFile(json, palette_tracker, block_tracker) {
+    block_tracker.id = 0;
     for (var i=0; i<json.length; i++) {
         var index = parseInt(json[i][0]);
         var factory = null;
         var block = null;
+
+        checkIDCount(block_tracker, index);
 
         if (json[i][1] instanceof Array){
             factory = palette_tracker.search_factory(json[i][1][0]);
@@ -29,7 +32,13 @@ function parseTAFile(json, palette_tracker) {
         }
 
         block = factory.make_block(factory.block_name, false);
+        block.block_id = index;
+        block.set_xy([json[i][2], json[i][3]]);
+    }
+}
 
-        alert(block);
+function checkIDCount(block_tracker, index){
+    if (block_tracker.id < index){
+        block_tracker.id = index;
     }
 }
