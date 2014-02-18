cebfc38a9b2fa61d4c63ea581552480a0d95d70b
Jorge Gómez <gomezlopez.jorge96@gmail.com>
2014-02-17 22:02:15 -0600

    Stack dock basic funcs part 1

--- ta_lib/docktracker.js
+++ ta_lib/docktracker.js
@@ -1,27 +1,11 @@
 function DockTracker(){
     this.dic = {};
+    this.special_types = ["clampn"];
 
-    var dock_descriptor = new DockDescriptor();
-    dock_descriptor.add_upper_dock([17, 1]);
-    dock_descriptor.add_lower_dock([17, 43]);
-    dock_descriptor.add_param_dock([115, 25]);
-    this.add_dock("basic1arg", dock_descriptor);
-
-    var dock_descriptor = new DockDescriptor();
-    dock_descriptor.add_param_dock([17, 25]);
-    this.add_dock("box", dock_descriptor);
-
-    var dock_descriptor = new DockDescriptor();
-    dock_descriptor.add_upper_dock([17, 1]);
-    dock_descriptor.add_lower_dock([17, 35]);
-    this.add_dock("basic", dock_descriptor);
-
-    var dock_descriptor = new DockDescriptor();
-    dock_descriptor.add_upper_dock([17, 1]);
-    dock_descriptor.add_lower_dock([17, 85]);
-    dock_descriptor.add_param_dock([115, 25]);
-    dock_descriptor.add_param_dock([115, 67]);
-    this.add_dock("basic2arg", dock_descriptor);
+    this.add_dock("basic1arg", this.make_dock_descriptor([17, 1], [17, 43], [[115, 25]], null));
+    this.add_dock("box", this.make_dock_descriptor(null, null, [[17, 25]], null));
+    this.add_dock("basic", this.make_dock_descriptor([17, 1], [17, 35], null, null));
+    this.add_dock("basic2arg", this.make_dock_descriptor([17, 1], [17, 85], [[115, 25], [115, 67]], null));
 }
 
 DockTracker.prototype = {
@@ -30,6 +14,32 @@ DockTracker.prototype = {
         this.dic[name] = dock;
     },
     get_dock: function(name){
+        if (this.special_types.indexOf(name) > -1){
+            if (name == "clampn"){
+            }
+        }
         return this.dic[name];
+    },
+    make_dock_descriptor: function(upper, lower, params, stack){
+        var dock_descriptor = new DockDescriptor();
+
+        if (upper != null){
+            dock_descriptor.add_upper_dock(upper);
+        }
+        if (lower != null){
+            dock_descriptor.add_lower_dock(lower);
+        }
+        if (params != null){
+            for (var i=0; i<params.length; i++){
+                dock_descriptor.add_param_dock(params[i]);
+            }
+        }
+        if (stack != null){
+            for (var i=0; i<stack.length; i++){
+                dock_descriptor.add_stack_dock(stack[i]);
+            }
+        }
+
+        return dock_descriptor;
     }
 }
--- ta_pal_desc/flow_palette_descriptor.js
+++ ta_pal_desc/flow_palette_descriptor.js
@@ -6,7 +6,9 @@ function FlowPaletteDesc(){
 FlowPaletteDesc.prototype = {
     constructor: FlowPaletteDesc,
     init_descriptor: function(){
-
+        var block_descriptor = new BlockDescriptor('block_res/clampn.svg', 'clampn', penup_block, null);
+        block_descriptor.add_label('Repeat', 5, 13, 19, 'Calibri', 'black');
+        this.descriptors['repeat_block'] = block_descriptor;
     },
     get_block_descriptor: function(name){
         return this.descriptors[name];
