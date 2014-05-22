ee6896b37c313aad5d6b3622c306001f02e3bedc
Jorge Gómez <gomezlopez.jorge96@gmail.com>
2014-05-21 21:38:46 -0600

    Pixel ratio set to 1

--- html_js/appinit.js
+++ html_js/appinit.js
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 
+Kinetic.pixelRatio = 1;
+
 var global_tracker = new GlobalVarsTracker();
 var user_vars_tracker = new UserVarsTracker();
 var block_tracker = new BlockTracker();
