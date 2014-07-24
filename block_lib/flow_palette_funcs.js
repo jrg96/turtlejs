5711950a88e2b263c479532b95e2a5b0d3abd8b4
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2014-07-24 15:31:26 -0600

    Repeat block default behavior

--- block_lib/flow_palette_funcs.js
+++ block_lib/flow_palette_funcs.js
@@ -14,14 +14,11 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 
 function repeat(params, values){
-    function inicia(){
-        if (params[2].stack_slots[0] != null){
-            for (var i=0; i<values[0][1]; i++){
-                params[2].stack_slots[0].chain_exec();
-            }
+    if (params[2].stack_slots[0] != null){
+        for (var i=0; i<values[0][1]; i++){
+            params[2].stack_slots[0].chain_exec();
         }
     }
-    inicia();
     return true;
 }
 
