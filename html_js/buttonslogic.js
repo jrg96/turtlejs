f5be9e470836ec66a8f9c9c73a4a3f575debbe3d
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2014-08-13 11:58:30 -0600

    Move scroll on resize window bugfix

--- html_js/buttonslogic.js
+++ html_js/buttonslogic.js
@@ -170,11 +170,10 @@ $(document).ready(function() {
     
     $(window).resize(function(){
         if (!MOBILE_VER){
-            remove_scrolls();
-            make_scrolls();
-            
             draw_stage.stage.height($(window).height() - 62);
             draw_stage.stage.width($(window).width() - 5);
+            remove_scrolls();
+            make_scrolls();
         } else{
             center_touch_bg();
         }
